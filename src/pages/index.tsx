04d505e4ac111cb0227f7065c05bce71c482b234
kelvearagao <kelvearagao@gmail.com>
2025-03-08 13:11:59 -0300

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
+import { useState } from "react";
+import * as XLSX from "xlsx";
 import Head from "next/head";
-import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
@@ -14,6 +15,28 @@ const geistMono = Geist_Mono({
 });
 
 export default function Home() {
+  const [data, setData] = useState([]);
+
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const binaryStr = e.target.result;
+      const workbook = XLSX.read(binaryStr, { type: "binary" });
+
+      // Supondo que você queira ler a primeira planilha
+      const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+      const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+
+      // Remove o cabeçalho e define o estado com os dados
+      const rows = jsonData;
+      setData(rows);
+    };
+    reader.readAsBinaryString(file);
+  };
+
   return (
     <>
       <Head>
@@ -26,91 +49,46 @@ export default function Home() {
         className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
       >
         <main className={styles.main}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js logo"
-            width={180}
-            height={38}
-            priority
-          />
-          <ol>
-            <li>
-              Get started by editing <code>src/pages/index.tsx</code>.
-            </li>
-            <li>Save and see your changes instantly.</li>
-          </ol>
-
-          <div className={styles.ctas}>
-            <a
-              className={styles.primary}
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                className={styles.logo}
-                src="/vercel.svg"
-                alt="Vercel logomark"
-                width={20}
-                height={20}
-              />
-              Deploy now
-            </a>
-            <a
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.secondary}
-            >
-              Read our docs
-            </a>
+          <div>
+            <h1>Upload de Arquivo XLSX</h1>
+            <br />
+            <input
+              type="file"
+              accept=".xlsx, .xls"
+              onChange={handleFileUpload}
+            />
+            <br />
+            <br />
+            <h2>Dados do Arquivo:</h2>
+            <br />
+            {data?.length > 0 && (
+              <table border={1}>
+                <thead>
+                  <tr>
+                    {data[0].map(
+                      (item, index) => index < 2 && <th key={item}>{item}</th>
+                    )}
+                  </tr>
+                </thead>
+                <tbody>
+                  {data
+                    .slice(1)
+                    .map(
+                      (row, index) =>
+                        row?.length > 0 && (
+                          <tr key={index}>
+                            {row.map(
+                              (row, colIndex) =>
+                                colIndex < 2 && <td key={colIndex}>{row}</td>
+                            )}
+                          </tr>
+                        )
+                    )}
+                </tbody>
+              </table>
+            )}
           </div>
         </main>
-        <footer className={styles.footer}>
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/file.svg"
-              alt="File icon"
-              width={16}
-              height={16}
-            />
-            Learn
-          </a>
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/window.svg"
-              alt="Window icon"
-              width={16}
-              height={16}
-            />
-            Examples
-          </a>
-          <a
-            href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/globe.svg"
-              alt="Globe icon"
-              width={16}
-              height={16}
-            />
-            Go to nextjs.org →
-          </a>
-        </footer>
       </div>
     </>
   );
